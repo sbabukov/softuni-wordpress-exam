8d2607fca8c41ce79882aed0610ee4ab07fae7ca
sbabukov <s.babukov@gmail.com>
2023-04-30 10:35:12 +0300

    Initial commit of header.php

---
+++ wp-content/themes/softuni-template-homes/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <link rel="profile" href="//gmpg.org/xfn/11" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+    
+    <title><?php wp_title(); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body>
+	<div class="site-wrapper">
+	<header class="site-header">
+		<?php if ( is_home() ): ?>
+			<h1 class="site-title"><a href="<?php echo esc_url( get_home_url()); ?>">Properties Offers</a></h1>
+			<?php else: ?>
+				<p class="site-title"><a href="<?php echo esc_url( get_home_url()); ?>">Properties Offers</a></p>
+			<?php endif; ?>	
+    </header>
+    
+        <!-- <header class="site-header">
+			<h1 class="site-title"><a href="<?php echo esc_url(get_home_url())?>">Properties Offers</a></h1>
+		</header> -->
