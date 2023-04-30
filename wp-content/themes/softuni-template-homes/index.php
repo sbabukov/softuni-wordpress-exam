f3cb076e99e13f65727743896c7a034529f4ca94
sbabukov <s.babukov@gmail.com>
2023-04-30 09:26:22 +0300

    Commit index.php and style.css of custom theme

---
+++ wp-content/themes/softuni-template-homes/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Properties</title>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+
+	<link rel="stylesheet" href="./css/master.css">
+	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+</head>
+<body>
+	<div class="site-wrapper">
+		<header class="site-header">
+			<h1 class="site-title"><a href="#">Properties Offers</a></h1>
+		</header>
+
+		<ul class="properties-listing">
+			<li class="property-card">
+				<div class="property-primary">
+					<h2 class="property-title"><a href="#">Two-bedroom apartment</a></h2>
+					<div class="property-meta">
+						<span class="meta-location">Ovcha Kupel, Sofia</span>
+						<span class="meta-total-area">Total area: 91.65 sq.m</span>
+					</div>
+					<div class="property-details">
+						<span class="property-price">€ 100,815</span>
+						<span class="property-date">Posted 14 days ago</span>
+					</div>
+				</div>
+				<div class="property-image">
+					<div class="property-image-box">
+						<img src="images/bedroom.jpg" alt="property image">
+					</div>
+				</div>
+			</li>
+
+			<li class="property-card">
+				<div class="property-primary">
+					<h2 class="property-title"><a href="#">Two-bedroom apartment</a></h2>
+					<div class="property-meta">
+						<span class="meta-location">Ovcha Kupel, Sofia</span>
+						<span class="meta-total-area">Total area: 91.65 sq.m</span>
+					</div>
+					<div class="property-details">
+						<span class="property-price">€ 100,815</span>
+						<span class="property-date">Posted 14 days ago</span>
+					</div>
+				</div>
+				<div class="property-image">
+					<div class="property-image-box">
+						<img src="images/bedroom.jpg" alt="property image">
+					</div>
+				</div>
+			</li>
+		</ul>
+		
+		<footer class="site-footer">
+			<p>&copy; Copyright 2023 | Developer links: 
+				<a href="/edits.html">Edits</a>,
+				<a href="/index.html">Home</a>,
+				<a href="/single.html">Single</a>
+			</p>
+		</footer>
+	</div>
+
+</body>
+</html>
