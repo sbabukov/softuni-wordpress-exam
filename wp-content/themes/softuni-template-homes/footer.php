f18ceb34084ac4e64e4e1fd83765f926f079ac18
sbabukov <s.babukov@gmail.com>
2023-04-30 10:36:41 +0300

    Initial commit footer.php

---
+++ wp-content/themes/softuni-template-homes/footer.php
@@ -0,0 +1,12 @@
+<footer class="site-footer">
+			<p>&copy; Copyright 2023 | Developer links: 
+				<a href="#">Edits</a>,
+				<a href="#">Home</a>,
+				<a href="#">Single</a>
+			</p>
+		</footer>
+	</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
