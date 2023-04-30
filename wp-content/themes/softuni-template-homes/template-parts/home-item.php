cdedcedf3199a0c26cefa56dea6e0e96c5f54322
sbabukov <s.babukov@gmail.com>
2023-04-30 10:38:33 +0300

    Initial commit of template-parts

---
+++ wp-content/themes/softuni-template-homes/template-parts/home-item.php
@@ -0,0 +1,40 @@
+
+<ul class="properties-listing">
+	<li class="property-card">
+		<div class="property-primary">
+			<h2 class="property-title"><a href="<?php get_permalink(); ?>">Two-bedroom apartment</a></h2>
+			<div class="property-meta">
+				<span class="meta-location">Ovcha Kupel, Sofia</span>
+				<span class="meta-total-area">Total area: 91.65 sq.m</span>
+			</div>
+			<div class="property-details">
+				<span class="property-price">€ 100,815</span>
+				<span class="property-date">Posted 14 days ago</span>
+			</div>
+		</div>
+		<div class="property-image">
+			<div class="property-image-box">
+				<img src="wp-content\themes\softuni-template-homes\assets\images\bedroom.jpg" alt="property image">
+			</div>
+		</div>
+	</li>
+
+	<li class="property-card">
+		<div class="property-primary">
+			<h2 class="property-title"><a href="#">Two-bedroom apartment</a></h2>
+			<div class="property-meta">
+				<span class="meta-location">Ovcha Kupel, Sofia</span>
+				<span class="meta-total-area">Total area: 91.65 sq.m</span>
+			</div>
+			<div class="property-details">
+				<span class="property-price">€ 100,815</span>
+				<span class="property-date">Posted 14 days ago</span>
+			</div>
+		</div>
+		<div class="property-image">
+			<div class="property-image-box">
+				<img src="wp-content\themes\softuni-template-homes\assets\images\bedroom.jpg" alt="property image">
+			</div>
+		</div>
+	</li>
+</ul>
